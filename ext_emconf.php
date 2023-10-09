405c54af67633f5b6ba42850c2884beb26578397
Stephan Ude <ude@mediatis.de>
2023-10-09 12:59:19 +0200

    [TASK] meta data

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Digital Marketing Framework - Distributor - Pardot',
-    'description' => 'Send form data to different target systems',
+    'description' => 'Send form data to Pardot via form handlers',
     'category' => 'be',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
